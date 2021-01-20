33b6eb3eca780dd7e29fd311890b3223afc72802
ledimari <ledi.huliganka@gmail.com>
2021-01-20 14:34:32 +0200

    add header and vertical parallax in header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-function init() {
-  // функция вызовет сама себя как только сайт загрузиться
+function handleHorisontalTransform(event) {
+  document.querySelectorAll('.parallax').forEach(parallax => {
+    let speed = parallax.getAttribute('data-speed');
+    let inverse = parallax.getAttribute('data-inverse');
+
+    const isInverse = inverse === "1" // нужно чтобы понимать добавлять ли минус
+    const cursorProgress = 100 / (window.innerWidth / event.clientX) // от 0 до 100
+
+    const offset = isInverse ? 10 : -10
+    const minMaxValue = isInverse ? (cursorProgress / 5) / speed : -(cursorProgress / 5) / speed
+
+    window.requestAnimationFrame(() => {
+      parallax.style.transform = `translateX(${minMaxValue - offset}px)`
+    })
+  });;
 }
 
-(init)();
+function handleMouseMove(event) {
+  handleHorisontalTransform(event);
+}
+
+(function() {
+  document.addEventListener('mousemove', handleMouseMove);
+})()
